cddb5ef5b698b78e9556f069e4479abe211f0157
Rob Pinder <rob.pinder@nextclimate.com>
2012-08-04 22:53:40 -0400

    Includes a user database that is initialized the first time a user logs in

--- nextclimate/query_zip.py
+++ nextclimate/query_zip.py
@@ -1,4 +1,3 @@
-
 import os
 from google.appengine.ext.webapp import template
 
@@ -11,6 +10,7 @@ import string
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
+from google.appengine.api import mail
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 # this class holds a zip code and climate info. This is an
@@ -26,9 +26,51 @@ class Zipcode(db.Model):
   maxTa1_2090 = db.IntegerProperty()    
   maxTb2_2090 = db.IntegerProperty()
 
+class UserNC(db.Model):
+    """ This is a NextClimate user """
+    name = db.StringProperty()
+    FBid = db.StringProperty()
+    gender = db.StringProperty()
+    email = db.EmailProperty()
+    birthday = db.DateProperty()
+    firstName = db.StringProperty()
+    lastName = db.StringProperty()
+    locale = db.StringProperty()
+    verified = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    lastLogin = db.DateTimeProperty(auto_now=True)
+    
+
 def zipcode_key(zipcode_value=None):
   """Constructs a datastore key for a Zipcode entity with zipcode_value."""
   return db.Key.from_path('Zipcode', zipcode_value or 'default_zipcode')
+
+def user_key(user_name=None):
+  """Constructs a datastore key for a Action entity with action_name."""
+  return db.Key.from_path('UserNC', user_name or 'default_action')
+
+def send_welcome_email(toEmail, toName):
+    """ This function sends a welcome email to new users """
+    message = mail.EmailMessage(sender="NextClimate <info@nextclimate.org>",
+				subject="Welcome to NextClimate")
+    message.to = toEmail
+    message.body = """
+    Dear %s,
+
+    We are glad you've signed up to be a NextClimate user. We
+    look forward to helping you and your community achieve your
+    climate change mitigation goals.
+
+    If you've found this service useful, let your friends know
+    by pointing them to www.nextclimate.org.
+
+    Your suggestions are welcome -- just hit reply to this email!
+
+    The NextClimate Team
+    """ % (toName)
+    message.send()
+
+
 
 
 # this class gets called when ther is a request to /zip_query
@@ -38,6 +80,51 @@ def zipcode_key(zipcode_value=None):
 # climate of a current location. Finally, this is packaged in the variable
 # named template and pushed to 
 class MainPage(webapp.RequestHandler):
+
+    def post(self):
+	# the welcome page does a form post with the user's info. This is
+	# implemented as a hidden form on the welcome page.
+	# the fields are populated from facebook data.
+	# Here, check to see if the user is already in our database and if
+	# not, add the user.
+	
+	# first check to see if this is a new users and if so add this user to
+	# the database
+	
+	user_query = UserNC.all()
+	user_query.filter("FBid = ",self.request.get('id'))
+
+	if not user_query.count(limit=1):
+	    # case where this id is not in the database,
+	    # so add this user
+
+	    new_user = UserNC(parent=user_key('99999'))
+
+	    new_user.name = self.request.get('name')
+	    new_user.FBid = self.request.get('id')
+	    new_user.gender = self.request.get('gender')
+	    new_user.email = db.Email(self.request.get('email'))
+
+	    try:
+		d = datetime.datetime.strptime(self.request.get('birthday'),"%m/%d/%Y").date()
+	    except:
+		d = None
+	    new_user.birthday = d
+	    new_user.firstName = self.request.get('first_name')
+	    new_user.lastName = self.request.get('last_name')
+	    new_user.locale = self.request.get('locale')
+	    new_user.verified = self.request.get('verified')
+	    #new_user.created = datetime.datetime.today()
+	    #new_user.lastLogin = datetime.datetime.today()
+	    
+	    new_user.put()
+
+	    send_welcome_email(new_user.email, new_user.name)
+	    # could put an else clause here to update the lastLogin.
+
+	# go back to the query_zip page
+	self.redirect('/queryZip')
+
 
     # execute this code at a get request
     def get(self):
